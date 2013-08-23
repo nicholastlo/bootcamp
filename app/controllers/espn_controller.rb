7f5f65c3e797edf4d750fa5ae2c146ebb55ed062
nicholas <nicholastclo@gmail.com>
2013-08-23 18:00:15 -0400

    added teams api

--- app/controllers/espn_controller.rb
+++ app/controllers/espn_controller.rb
@@ -15,9 +15,23 @@ class EspnController < ApplicationController
 		@data_in_json = parsed_json = ActiveSupport::JSON.decode(@data)
 	end
 
-	def news
+	def athlete_news
 		@data = Net::HTTP.get(URI.parse("http://api.espn.com/v1/sports/#{params[:sport]}/#{params["league"]}/athletes/#{params[:athlete_id]}/news?apikey=zcuvb9y2rb7hjzqmfd7fgbsn"))
 		@data_in_json = parsed_json = ActiveSupport::JSON.decode(@data)
 	end
 
+	def teams
+		if params[:team_id]
+			@data = Net::HTTP.get(URI.parse("http://api.espn.com/v1/sports/#{params[:sport]}/#{params["league"]}/teams/#{params[:team_id]}?apikey=zcuvb9y2rb7hjzqmfd7fgbsn"))
+		else
+			@data = Net::HTTP.get(URI.parse("http://api.espn.com/v1/sports/#{params[:sport]}/#{params["league"]}/teams?apikey=zcuvb9y2rb7hjzqmfd7fgbsn"))
+		end
+
+		@data_in_json = parsed_json = ActiveSupport::JSON.decode(@data)
+	end
+
+	def team_news
+		@data = Net::HTTP.get(URI.parse("http://api.espn.com/v1/sports/#{params[:sport]}/#{params["league"]}/teams/#{params[:team_id]}/news?apikey=zcuvb9y2rb7hjzqmfd7fgbsn"))
+		@data_in_json = parsed_json = ActiveSupport::JSON.decode(@data)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Bootcamp::Application.routes.draw do
   root :to => 'espn#index'
   match 'athletes/sport/:sport/league/:league/(athlete_id/:athlete_id)'          => 'espn#athletes'
-  match 'athletes/sport/:sport/league/:league/athlete_id/:athlete_id/news'       => 'espn#news'
+  match 'athletes/sport/:sport/league/:league/athlete_id/:athlete_id/news'       => 'espn#athlete_news'
+  match 'teams/sport/:sport/league/:league/(team_id/:team_id)'                   => 'espn#teams'
+  match 'teams/sport/:sport/league/:league/team_id/:team_id/news'                => 'espn#team_news'
 end
