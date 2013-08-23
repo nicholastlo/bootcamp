5f4e94ebef616337e7899567992db0ccabcfce37
nicholas <nicholastclo@gmail.com>
2013-08-23 17:11:05 -0400

    added default page

--- app/controllers/espn_controller.rb
+++ app/controllers/espn_controller.rb
@@ -2,6 +2,9 @@ require 'net/http'
 require 'json'
 
 class EspnController < ApplicationController
+	def index
+	end
+
 	def athletes
 		if params[:athlete_id]
 			@data = Net::HTTP.get(URI.parse("http://api.espn.com/v1/sports/#{params[:sport]}/#{params["league"]}/athletes/#{params[:athlete_id]}?apikey=zcuvb9y2rb7hjzqmfd7fgbsn"))
@@ -16,4 +19,5 @@ class EspnController < ApplicationController
 		@data = Net::HTTP.get(URI.parse("http://api.espn.com/v1/sports/#{params[:sport]}/#{params["league"]}/athletes/#{params[:athlete_id]}/news?apikey=zcuvb9y2rb7hjzqmfd7fgbsn"))
 		@data_in_json = parsed_json = ActiveSupport::JSON.decode(@data)
 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Bootcamp::Application.routes.draw do
+  root :to => 'espn#index'
   match 'athletes/sport/:sport/league/:league/(athlete_id/:athlete_id)'          => 'espn#athletes'
   match 'athletes/sport/:sport/league/:league/athlete_id/:athlete_id/news'       => 'espn#news'
- 
 end
